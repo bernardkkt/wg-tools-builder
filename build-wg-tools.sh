b751a0deb81567f71c7d0d25dd6c87bc065b9a35
bernardkkt <kianshunkin@gmail.com>
2020-05-23 20:31:50 +0800

    Create build-wg-tools.sh

---
+++ build-wg-tools.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Print everytime when a line is executed; exit if fails
+set -x
+set -e
+
+# Initialise build environment
+git clone --depth=1 https://git.zx2c4.com/wireguard-tools
+cd wireguard-tools/src
+
+# Build
+make wg
+
+# Artefacts
+ls wg
+ls wg-quick/linux.bash
